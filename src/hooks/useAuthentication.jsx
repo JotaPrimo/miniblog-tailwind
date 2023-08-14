b2d32bd9ba88f02b64e90e38922c9c14c5a7b96b
sistemasimcg <sistemas@institutomirim.org.br>
2023-08-14 14:13:46 -0300

    130. Registrando usuário no sistema

--- src/hooks/useAuthentication.jsx
+++ src/hooks/useAuthentication.jsx
@@ -9,44 +9,106 @@ import {
 import { useState, useEffect } from "react";
 
 export const useAuthentication = () => {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
 
-  // dois estados possiveis: erro e loading
-  const [erro, setErro] = useState();
-  const [loading, setLoading] = useState()
+  // deal with memory leak
+  const [cancelled, setCancelled] = useState(false);
 
-  //cleanup -> limpar as funções, problema de memory leaky
-  const [canceled, setCancelled] = useState()
+  const auth = getAuth();
 
-  const auth = getAuth()
-
-  function checkIfIsCanceled(params) {
-    // evita o vazamento de memoria
-    if (canceled) {
+  function checkIfIsCancelled() {
+    if (cancelled) {
       return;
     }
   }
 
-  // funcao para criar novo usuario
-  // funcao assincrona, tem que verificar se deu problema e foi cancelada, setar o loadin e tentar cadastrar
   const createUser = async (data) => {
-    checkIfIsCanceled();
+    checkIfIsCancelled();
 
     setLoading(true);
 
     try {
-      const {user} = await createUserWithEmailAndPassword(
-        auth, 
+      const { user } = await createUserWithEmailAndPassword(
+        auth,
         data.email,
         data.password
-      )
+      );
 
-        await updateProfile(user, {
-          displayName: data.displayName
-        })
+      await updateProfile(user, {
+        displayName: data.displayName,
+      });
 
+      return user;
     } catch (error) {
+      console.log(error.message);
+      console.log(typeof error.message);
 
+      let systemErrorMessage;
+
+      if (error.message.includes("Password")) {
+        systemErrorMessage = "A senha precisa conter pelo menos 6 caracteres.";
+      } else if (error.message.includes("email-already")) {
+        systemErrorMessage = "E-mail já cadastrado.";
+      } else {
+        systemErrorMessage = "Ocorreu um erro, por favor tenta mais tarde.";
+      }
+
+      setError(systemErrorMessage);
     }
-  }
 
-}
+    setLoading(false);
+  };
+
+  const logout = () => {
+    checkIfIsCancelled();
+
+    signOut(auth);
+  };
+
+  const login = async (data) => {
+    checkIfIsCancelled();
+
+    setLoading(true);
+    setError(false);
+
+    try {
+      await signInWithEmailAndPassword(auth, data.email, data.password);
+    } catch (error) {
+      console.log(error.message);
+      console.log(typeof error.message);
+      console.log(error.message.includes("user-not"));
+
+      let systemErrorMessage;
+
+      if (error.message.includes("user-not-found")) {
+        systemErrorMessage = "Usuário não encontrado.";
+      } else if (error.message.includes("wrong-password")) {
+        systemErrorMessage = "Senha incorreta.";
+      } else {
+        systemErrorMessage = "Ocorreu um erro, por favor tenta mais tarde.";
+      }
+
+      console.log(systemErrorMessage);
+
+      setError(systemErrorMessage);
+    }
+
+    console.log(error);
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    return () => setCancelled(true);
+  }, []);
+
+  return {
+    auth,
+    createUser,
+    error,
+    logout,
+    login,
+    loading,
+  };
+};
