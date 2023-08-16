d4d480f84eb90aedf32711f224a4ea8a42e25ea5
JotaPrimo <gestald118@gmail.com>
2023-08-15 21:51:08 -0400

    commit antes do merge

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -4,17 +4,18 @@ import InputText from "../../components/Inputs/InputText";
 const Login = () => {
   return (
     <div className="card sm:w-6 md:w-1/3 mx-auto">
-        
-    <div>
-      <h1>Login</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-    </div>
+      <div className="flex justify-center">
+        <div>
+          <h1 className="text-center">Login</h1>
+          <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+        </div>
+      </div>
 
-    <form action="#">
-      <InputText />
-      <InputText />
-    </form>
-  </div>
+      <form action="#" className="mt-5">
+        <InputText label={"Email"} placeholder={"Informe seu email"} />
+        <InputText label={"Password"} placeholder={"Informe sua senha"} />
+      </form>
+    </div>
   );
 };
 
